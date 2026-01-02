65f7bdc76a43a0e3a3bad4717cab842dc972cd5c
Eysteix <vision060607@gmail.com>
2026-01-02 19:45:20 +0000

    Started Upgrading my neovim config for 2026

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,29 +1,29 @@
 return {
-	-- {
-	-- 	"zbirenbaum/copilot.lua",
-	-- 	dependencies = {
-	-- 		"copilotlsp-nvim/copilot-lsp",
-	-- 		enabled = false,
-	-- 		init = function()
-	-- 			vim.g.copilot_nes_debounce = 500
-	-- 		end,
-	-- 	},
-	-- 	enabled = false,
-	-- 	cmd = "Copilot",
-	-- 	event = "InsertEnter",
-	-- 	config = function()
-	-- 		require("copilot").setup({
-	-- 			nes = {
-	-- 				enabled = true,
-	-- 				keymap = {
-	-- 					accept_and_goto = "<leader>p",
-	-- 					accept = false,
-	-- 					dismiss = "<Esc>",
-	-- 				},
-	-- 			},
-	-- 		})
-	-- 	end,
-	-- },
+	{
+		"zbirenbaum/copilot.lua",
+		dependencies = {
+			"copilotlsp-nvim/copilot-lsp",
+			enabled = false,
+			init = function()
+				vim.g.copilot_nes_debounce = 500
+			end,
+		},
+		enabled = false,
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				nes = {
+					enabled = true,
+					keymap = {
+						accept_and_goto = "<leader>p",
+						accept = false,
+						dismiss = "<Esc>",
+					},
+				},
+			})
+		end,
+	},
 	{
 		"CopilotC-Nvim/CopilotChat.nvim",
 		dependencies = {
