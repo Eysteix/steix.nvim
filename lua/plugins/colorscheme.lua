22ca8e2d0787cb31b30fa82c934066087070de6b
Eysteix <vision060607@gmail.com>
2026-03-14 20:19:32 +0000

    Made some color changes and lualine upgrades

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -63,7 +63,10 @@ return {
 	},
 	{
 		"olimorris/onedarkpro.nvim",
-		priority = 1000, -- Ensure it loads first
+		priority = 1000,
+		opts = {
+			transparent_background = true
+		}
 	},
 	{
 		"audibleblink/hackthebox.vim",
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -40,38 +40,8 @@ return {
 				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = {
 					"filename",
-					-- Copilot status
-					{
-						function()
-							return " "
-						end,
-						color = function()
-							local status = require("sidekick.status").get()
-							if status then
-								return status.kind == "Error" and "DiagnosticError"
-										or status.busy and "DiagnosticWarn"
-										or "Special"
-							end
-						end,
-						cond = function()
-							return require("sidekick.status").get() ~= nil
-						end,
-					},
 				},
 				lualine_x = {
-					-- CLI session status
-					{
-						function()
-							local status = require("sidekick.status").cli()
-							return " " .. (#status > 1 and #status or "")
-						end,
-						cond = function()
-							return #require("sidekick.status").cli() > 0
-						end,
-						color = function()
-							return "Special"
-						end,
-					},
 					"encoding",
 					"fileformat",
 					"filetype",
